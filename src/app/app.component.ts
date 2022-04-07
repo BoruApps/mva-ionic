4ae726d26a88c0da379e810f4a49c9e9efd4f557
rahul prajapati <prajapatirahul322@gmail.com>
2022-04-07 19:55:10 +0530

    52550_login_page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {ToastController, NavController} from '@ionic/angular';
+import {HttpHeaders, HttpClient} from '@angular/common/http';
+import {LoadingController} from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
+import {AppConstants} from "./providers/constant/constant";
 import { Platform } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -37,25 +43,44 @@ export class AppComponent {
       icon: 'document'
     },{
       title: 'Profile',
-      url: 'home',
+      url: 'profile',
       icon: 'person'
     },{
       title: 'Logout',
-      url: 'home',
+      url: 'logout',
       icon: 'log-out'
     },
   ];
+
   constructor(
-    private platform: Platform,
-    private statusBar: StatusBar,
-    private splashScreen: SplashScreen,
+      private router: Router,
+      public storage: Storage,
+      public toastController: ToastController,
+      private httpClient: HttpClient,
+      public appConst: AppConstants,
+      private navCtrl: NavController,
+      public loadingController: LoadingController,
+      private platform: Platform,
+      private statusBar: StatusBar,
+      private splashScreen: SplashScreen,
   ) {
     this.initializeApp();
   }
+
+  async ngOnInit() {
+    await this.storage.create();
+  }
+  
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+  
+  logoutUser(){
+      console.log('logout clicked');
+      this.storage.set("userdata", null);
+      this.router.navigateByUrl('/');
   }
 }
